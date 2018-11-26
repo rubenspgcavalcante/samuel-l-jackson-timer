39b83eed88e0c0069343daf6624a54c33cd7c564
Rubens Pinheiro Goncalves Cavalcante <rubenspgcavalcante@gmail.com>
2018-11-26 20:15:39 +0100

    Locking timer

--- src/server/actions/countdown.js
+++ src/server/actions/countdown.js
@@ -1,13 +1,19 @@
 const { setCurrentTimer } = require("../../commons/actions/index");
 
-const startCountDown = (namespace, startOn) => (dispatch, getState) => {
-  dispatch(setCurrentTimer(namespace, startOn));
+const intervals = {};
 
-  const intervalId = setInterval(() => {
+const startCountDown = (namespace, startOn) => (dispatch, getState) => {
+  if (intervals[namespace]) return false;
+
+  dispatch(setCurrentTimer(namespace, startOn));  
+  intervals[namespace] = setInterval(() => {
     const updated = getState().timer.rooms[namespace] - 1;
     dispatch(setCurrentTimer(namespace, updated));
 
-    if (updated === 0) return clearInterval(intervalId);
+    if (updated === 0) {
+      clearInterval(intervals[namespace]);
+      intervals[namespace] = null;
+    }
   }, 1000);
 };
 
